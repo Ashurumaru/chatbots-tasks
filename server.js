8c006d9ca2b5163a028bb4d47b28d2c9ba547396
Ashurumaru <86610969+Ashurumaru@users.noreply.github.com>
2024-02-09 21:45:15 +0800

    task2

--- server.js
+++ server.js
@@ -3,8 +3,31 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/", function(request, response){
-    response.send('<h1>Привет, Октагон!</h1>');
+app.get("/static", function(_, response){
+  const responseData = {
+      header: "Hello",
+      body: "Octagon NodeJS Test"
+  };
+  response.json(responseData);
+});
+
+app.get("/dynamic", function(request, response){
+  const a = parseInt(request.query.a);
+  const b = parseInt(request.query.b);
+  const c = parseInt(request.query.c);
+  if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    const errorResponse = {
+        header: "Error"
+    };
+    response.json(errorResponse);
+  } else {
+    const result = (a * b * c) / 3;
+    const responseData = {
+        header: "Calculated",
+        body: result
+    };
+  response.json(responseData);
+  }
 });
 
 app.listen(PORT, () => {
